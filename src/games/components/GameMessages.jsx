fbb1a8852606c037a94615658737e69c4fd55de5
IngenAparicio <ingen.aparicio@gmail.com>
2025-02-10 09:24:30 -0500

    send message from input

--- src/games/components/GameMessages.jsx
+++ src/games/components/GameMessages.jsx
@@ -1,14 +1,11 @@
-import { useMemo } from "react";
-import { getMessageByGameId } from "../helpers/getMessageByGameId";
 
-
-export const GameMessages = ({gameId}) => {
-  const messages = useMemo( () => getMessageByGameId(gameId), [gameId]);
+export const GameMessages = ({gameMessage}) => {
+  
   return (
     <div className="row rows-cols-1 rows-col-md-3 g-3">
       {
-        messages.map( mes => (
-          <textarea className="form-control mt-3" readOnly key={mes.id}>{mes.text}</textarea>
+        gameMessage.map( mes => (
+          <input type="textarea" className="form-control mt-3" readOnly key={mes.id} value={mes.text}/>
         ))
       }
     </div>
--- src/games/pages/DetailPage.jsx
+++ src/games/pages/DetailPage.jsx
@@ -1,13 +1,40 @@
 import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getGameById, getMessageByGameId } from '../helpers'
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { GameMessages } from "../components/GameMessages";
+
+
 
 export const DetailPage = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
-  const game = useMemo( () => getGameById(id), [id]);
+  const game = useMemo(() => getGameById(id), [id]);
+
+  const gameMessage = useMemo(() => getMessageByGameId(id), [id]);
+
+  let messageTemp = {
+    id: 'm-8',
+    gameId: game.id,
+    text: '',
+  }
+
+  const [texto, setTexto] = useState('');
+
+  const [MessageList, setMessageList] = useState(gameMessage);
+
+  const onAddText = (event) => {
+    event.preventDefault();
+    if (texto.trim() !== '') {
+      messageTemp.text = texto.trim();
+      setMessageList((MessageList) => [...MessageList, messageTemp]); // Agregar el texto a la lista
+      setTexto(''); // Limpiar el input
+    }
+  };
+
+  const onInputChance = (evento) => {
+    setTexto(evento.target.value);
+  };
 
   const onNavigateBack = () => {
     navigate(-1);
@@ -19,45 +46,53 @@ export const DetailPage = () => {
 
   return (
     <>
-    <div className="row mt-5">
-      <div className="col-4">
-        <img src={`/games/${id}.jpeg`} alt={game.name} className="img-thumbnail" />
+      <div className="row mt-5">
+        <div className="col-4">
+          <img src={`/games/${id}.jpeg`} alt={game.name} className="img-thumbnail" />
+        </div>
+        <div className="col-8">
+          <h3>{game.name}</h3>
+          <ul className="list-group list-group-flush mt-3">
+            <li className="list-group-item"><b> Category:</b> {game.category}</li>
+            <li className="list-group-item"><b> Price:</b> {game.price}</li>
+            <li className="list-group-item"><b> Rating:</b> {game.rating} <i class="bi bi-star"></i></li>
+          </ul>
+          <h5 className="mt-3">{game.description}</h5>
+          <button className="btn btn-primary m-3">
+            Buy
+          </button>
+          <button className="btn btn-primary m-3"
+            onClick={onNavigateBack}>
+            close
+          </button>
+        </div>
       </div>
-      <div className="col-8">
-        <h3>{game.name}</h3>
-        <ul className="list-group list-group-flush mt-3">
-          <li className="list-group-item"><b> Category:</b> {game.category}</li>
-          <li className="list-group-item"><b> Price:</b> {game.price}</li>
-          <li className="list-group-item"><b> Rating:</b> {game.rating} <i class="bi bi-star"></i></li>
-        </ul>
-        <h5 className="mt-3">{game.description}</h5>
-        <button className="btn btn-primary m-3">
-          Buy
-        </button>
-        <button className="btn btn-primary m-3"
-          onClick={onNavigateBack}>
-          close
-        </button>
+      <div className="row mt-5">
+        <div className="col-12">
+          <form onSubmit={onAddText}>
+            <input
+              type="textarea"
+              placeholder="Leave a Message.."
+              className="form-control"
+              value={texto}
+              onChange={onInputChance}
+            />
+          </form>
+        </div>
       </div>
-    </div>
-    <div className="row mt-5">
-      <div className="col-12">
-        <textarea name="" id="" placeholder="Leave a Message.." className="form-control"></textarea>
+      <div className="row mt-2">
+        <div className="col-12 d-flex justify-content-end">
+          <button className="btn btn-primary m-3" onClick={onAddText}>Send</button>
+        </div>
       </div>
-    </div>
-    <div className="row mt-2">
-      <div className="col-12 d-flex justify-content-end">
-        <button className="btn btn-primary m-3">Send</button>
+      <div className="row mt-5">
+        <h3>Messages:</h3>
       </div>
-    </div>
-    <div className="row mt-5">
-      <h3>Messages:</h3>
-    </div>
-    <div className="row mt5">
-      {        
-          <GameMessages  gameId={game.id}/>        
-      }
-    </div>
+      <div className="row mt-3 mb-5">
+        {
+          <GameMessages gameMessage={MessageList} />
+        }
+      </div>
     </>
   )
 }
